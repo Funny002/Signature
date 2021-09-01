d900a240fe2b65e1e6c08157f4d64cd0fb998c32
Funny <2048412928@qq.com>
2021-09-01 16:11:22 +0800

    我太菜了
    我太菜了

--- js/__base.js
+++ js/__base.js
@@ -34,24 +34,6 @@ function Throttle (func, timeout = 1) {
 }
 
 /**
- * 防抖
- * @param { Function } func
- * @param { Number } timeout
- * @returns { (...arg:[*]) => void }
- * @constructor
- */
-function Debounce (func, timeout = 1) {
-  let state = undefined
-  return function (...arg) {
-    if (state !== undefined) clearTimeout(state)
-    state = setTimeout(() => {
-      state = undefined
-      func(...arg)
-    }, timeout)
-  }
-}
-
-/**
  *
  * @param { Number } val
  * @param { Number } max
@@ -60,29 +42,4 @@ function Debounce (func, timeout = 1) {
  */
 function SizeLimit (val, max, min) {
   return Math.min(Math.max(val, min), max)
-}
-
-/**
- *
- * @param { CanvasRenderingContext2D } context
- * @param { {x:Number, y:Number} } p0
- * @param { number } size
- * @param { (CanvasRenderingContext2D) => void } func
- */
-function DrawPoint (context, p0, size = 2, func = undefined) {
-  // const diff = (size / 2)
-  context.beginPath();
-  context.arc(p0.x, p0.y, size, 0, 2 * Math.PI, true)
-  context.fillStyle = '#000'
-  if (func) func(context)
-  context.fill()
-  context.closePath();
-}
-
-function DrawPointLine (p0, p1, func) {
-  const count = firstOrderGenerate(p0, p1)
-  const t = 1 / count
-  for (let i = 0; i <= count; i++) {
-    func(firstOrder(p0, p1, SizeLimit(t * i, 1, 0)))
-  }
 }
--- js/main.js
+++ js/main.js
@@ -1,68 +1,110 @@
-const Ratio = 2; // 画布固定为 2
-let view, Canvas;
+const Ratio = 2;
+let view, Canvas, DrawPoint;
+
 $(function () {
-  /*  画板  */
   view = $('#canvas');
   const {offsetWidth, offsetHeight} = view;
   view.setAttribute('width', offsetWidth * Ratio + 'px');
   view.setAttribute('height', offsetHeight * Ratio + 'px');
   Canvas = view.getContext('2d');
+  DrawPoint = (context => function (point, size, func = undefined) {
+    context.beginPath();
+    context.arc(point.x, point.y, size, 0, 2 * Math.PI, true)
+    context.fillStyle = '#000'
+    if (func) func(context)
+    context.fill()
+    context.closePath();
+  })(Canvas);
   /*  方法监听  */
   // 按下
-  view.addEventListener('mousedown', e => toSize(e, startFunc));
+  view.addEventListener('mousedown', startFunc);
   // 移出
-  view.addEventListener('mouseout', e => toSize(e, exitFunc));
+  view.addEventListener('mouseout', exitFunc);
   // 弹起
-  view.addEventListener('mouseup', e => toSize(e, exitFunc));
+  view.addEventListener('mouseup', exitFunc);
 })
-let state = false
+// 曲线优化 0 -> 0.5, 最好 0.5
+let Curve = 0.5
+let FuncState = false
+let Point0, Point0_0, Point0_1;
 // 测试最低 0.3
-let drawSize = SizeLimit(10, Infinity, 0.3)
+const DrawSizeMin = 0.3
+const DrawSizeMax = SizeLimit(5, Infinity, DrawSizeMin)
 
-/**
- *
- * @param {*} event
- * @param {({y:number, x:number}) => void} func
- */
-function toSize (event, func) {
-  const {layerY, layerX} = event
-  func({
+let DrawSize = s => SizeLimit(isNaN(s) ? 0 : s, DrawSizeMax, DrawSizeMin)
+let DrawSizeLink = DrawSizeMax
+
+////////////////////////////////////////////////////////////////////
+function DrawPointLine (p0, p1, s) {
+  const count = firstOrderGenerate(p0, p1)
+  const hasSize = DrawSizeLink
+  const t = 1 / count
+  for (let i = 0; i <= count; i++) {
+    DrawSizeLink = DrawSize(hasSize + (s * t * i))
+    DrawPoint(firstOrder(p0, p1, SizeLimit(t * i, 1, 0)), DrawSizeLink)
+  }
+}
+
+function DrawPointCurve (p0, p1, p2, s) {
+  const count = secondOrderGenerate(p0, p1, p2)
+  const hasSize = DrawSizeLink
+  const t = 1 / count
+  for (let i = 0; i <= count; i++) {
+    DrawSizeLink = DrawSize(hasSize + (s * t * i))
+    DrawPoint(secondOrder(p0, p1, p2, SizeLimit(t * i, 1, 0)), DrawSizeLink)
+  }
+}
+
+function toPoint ({layerY, layerX}) {
+  return {
     t: new Date().getTime(),
     y: SizeLimit(layerY * Ratio, Infinity, 0),
     x: SizeLimit(layerX * Ratio, Infinity, 0)
-  })
+  }
 }
 
-let lestPoint
-
-function startFunc (Point) {
-  state = true
-  lestPoint = Point
-  DrawPoint(Canvas, Point, drawSize)
-  console.log('startFunc ->>', Point)
+function startFunc (event) {
+  AddPoint(event)
+  FuncState = true
   view.addEventListener('mousemove', moveFunc)
 }
 
-function exitFunc (Point) {
-  if (state) {
-    state = false
-    lestPoint = null
-    DrawPoint(Canvas, Point, drawSize)
-    // console.log('exitFunc ->>', Point)
+function exitFunc (event) {
+  if (FuncState) {
+    AddPoint(event)
+    FuncState = false
+    DrawSizeLink = DrawSizeMax
+    Point0 = Point0_0 = Point0_1 = null;
     view.removeEventListener('mousemove', moveFunc)
   }
 }
 
-// 移动
-function moveFunc (event) {
-  toSize(event, function (Point) {
-    if (!lestPoint) {
-      DrawPoint(Canvas, Point, drawSize)
+const throttleTimeout = 10
+const moveFunc = Throttle(AddPoint, throttleTimeout)
+
+/**
+ * 不知道用什么表达
+ */
+function AddPoint (event) {
+  const p1 = toPoint(event);
+  if (!Point0) {
+    DrawPoint(p1, DrawSizeMin);
+  } else {
+    ////////////////////////////// 是我太菜了，暂不知道用什么计算
+    const distance = firstOrderGenerate(Point0, p1) / Ratio;
+    let difS = distance / 100
+    console.log(difS, distance)
+    difS = SizeLimit(difS > 1 ? -difS * 2 : difS, Infinity, -(DrawSizeMax / (DrawSizeMax / Ratio)))
+    ////////////////////////////// 是我太菜了，暂不知道用什么计算
+    const p0_1 = firstOrder(Point0, p1, 1 - Curve);
+    const p0_0 = Curve === 0.5 ? p0_1 : firstOrder(Point0, p1, Curve);
+    if (!Point0_1) {
+      DrawPointLine(Point0, p0_1, difS)
     } else {
-      DrawPointLine(lestPoint, Point, function (valPoint) {
-        DrawPoint(Canvas, valPoint, drawSize)
-      })
+      DrawPointCurve(Point0_1, Point0, p0_0, difS)
+      if (Curve !== 0.5) DrawPointLine(p0_0, p0_1, difS)
     }
-    lestPoint = Point
-  })
+    [Point0_0, Point0_1] = [p0_0, p0_1];
+  }
+  Point0 = p1;
 }
